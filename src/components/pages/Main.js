3308ded61c5672b94b3752af069d4a6063353037
tatyana <sazonko.tatyana@gmail.com>
2023-05-28 16:42:41 +0300

    before second branch for new test

---
+++ src/components/pages/Main.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import { shops1 } from '../data1'
+
+export default function Main({shop, setShop}) {
+  return (
+    <main>
+        <div className='main'>
+            <div className='sidebar'>
+                {shop.map((el) =>{
+                    
+                })}
+            </div>
+            <div className='sidebar right'>
+
+            </div>
+        </div>
+    </main>
+  )
+}
